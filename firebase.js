c2ea6763c3cf3020a7dadbe4f108b8da9ccd9879
Mopnh <Mmwwmr636@gmail.com>
2026-01-24 10:35:33 +0200

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.2.0/firebase-app.js";
+import { getDatabase, ref, push, remove, onValue } from "https://www.gstatic.com/firebasejs/10.2.0/firebase-database.js";
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyBbMdjtZ5xUpiUDl0EgtO7Ek9d7-12-tAw",
+  authDomain: "om-nosa-baklava.firebaseapp.com",
+  databaseURL: "https://om-nosa-baklava-default-rtdb.firebaseio.com",
+  projectId: "om-nosa-baklava",
+  storageBucket: "om-nosa-baklava.firebasestorage.app",
+  messagingSenderId: "768849634836",
+  appId: "1:768849634836:web:f961dc50cf2caea9ab2901"
+};
+
+export const app = initializeApp(firebaseConfig);
+export const db = getDatabase(app);
+export { ref, push, remove, onValue };

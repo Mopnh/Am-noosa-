b18be4fffe41ca8f3901b5dfdb8add4634441275
Mopnh <Mmwwmr636@gmail.com>
2026-01-24 13:01:45 +0200

    Update firebase.js
    تعديلات

--- firebase.js
+++ firebase.js
@@ -1,18 +1,12 @@
-// استبدل القيم بالمعلومات الخاصة بمشروع Firebase الخاص بك
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-app.js";
-import { getDatabase, ref, onValue } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-database.js";
-
-const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  databaseURL: "https://YOUR_PROJECT_ID-default-rtdb.firebaseio.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
-};
-
-const app = initializeApp(firebaseConfig);
-const db = getDatabase(app);
-
-export { db, ref, onValue };
+import { db, ref, onValue } from './firebase.js';
+const container = document.getElementById("productsContainer");
+onValue(ref(db, "products"), snapshot => {
+    container.innerHTML = ""; // تفريغ المحتوى
+    snapshot.forEach(child => {
+        const prod = child.val();
+        const card = document.createElement("div");
+        card.className = "card";
+        card.innerHTML = `<img src="${prod.image}"><div class="overlay"><div><h3>${prod.name}</h3><a href="https://wa.me/218928486845">اطلب الآن</a></div></div>`;
+        container.appendChild(card);
+    });
+});

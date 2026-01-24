24b5f819523d8578d4ca94eba59045e6da94603b
Mopnh <Mmwwmr636@gmail.com>
2026-01-24 12:06:10 +0200

    Update firebase.js
    تعديلات

--- firebase.js
+++ firebase.js
@@ -1,16 +1,18 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.2.0/firebase-app.js";
-import { getDatabase, ref, push, remove, onValue } from "https://www.gstatic.com/firebasejs/10.2.0/firebase-database.js";
+// استبدل القيم بالمعلومات الخاصة بمشروع Firebase الخاص بك
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-app.js";
+import { getDatabase, ref, onValue } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-database.js";
 
-export const firebaseConfig = {
-  apiKey: "AIzaSyBbMdjtZ5xUpiUDl0EgtO7Ek9d7-12-tAw",
-  authDomain: "om-nosa-baklava.firebaseapp.com",
-  databaseURL: "https://om-nosa-baklava-default-rtdb.firebaseio.com",
-  projectId: "om-nosa-baklava",
-  storageBucket: "om-nosa-baklava.firebasestorage.app",
-  messagingSenderId: "768849634836",
-  appId: "1:768849634836:web:f961dc50cf2caea9ab2901"
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+  databaseURL: "https://YOUR_PROJECT_ID-default-rtdb.firebaseio.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT_ID.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
 };
 
-export const app = initializeApp(firebaseConfig);
-export const db = getDatabase(app);
-export { ref, push, remove, onValue };
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+export { db, ref, onValue };
